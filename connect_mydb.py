83ca4b447fc2c53031c82d42662e1698988eb42d
guiming <guiming1988@126.com>
2021-07-30 13:49:41 +0800

    测试Pycharm提交
    Signed-off-by: guiming <guiming1988@126.com>

---
+++ connect_mydb.py
@@ -0,0 +1,14 @@
+# caching_sha2_password登录失败解决方法：更改默认登录方式+引入mysql-connector-python
+
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="root" ,
+  database = "mydatabase"
+)
+cursor = mydb.cursor()
+print(mydb)
+print(cursor)
+cursor.close()
+mydb.close()
